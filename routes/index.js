cb90433f3198059fe7bc8c73534dc1c12fc48b95
Unstable <unstablectrl@gmail.com>
2017-05-08 23:02:37 +0100

    Add PostgreSQL client for Node.js

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const pool = require('../lib/db');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+  pool.query('SELECT $1::int AS number', ['3'])
+  .then((res) => {
+    console.log('number:', res.rows[0].number);
+  })
+  .catch((err) => {
+    console.error('error running query', err);
+  });
+  
   res.render('index', { title: 'Express' });
 });
 

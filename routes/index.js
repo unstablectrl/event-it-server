170dad0b5a8fac4cb5ad554a51809d38aa198e7f
Unstable <unstablectrl@gmail.com>
2017-05-09 19:38:58 +0100

    Add simple database test (select and insert) and display results

--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,25 @@ const pool = require('../lib/db');
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  pool.query('SELECT brand FROM "BRAND"', [])
-  .then((dbres) => {
-    res.render('index', { title: dbres.rows[0].brand });
-  })
-  .catch((err) => {
-    console.error('error running query', err);
-  });
-  
+  pool.query('SELECT * FROM "BRAND"', [])
+    .then((dbres) => {
+      res.render('index', { title: 'Brands', brands: dbres.rows });
+    })
+    .catch((err) => {
+      console.error('error running query', err);
+    });
+});
+
+router.post('/add', function(req, res, next) {
+  console.log('form query parameters: ', req.body);
+  pool.query('INSERT INTO "BRAND"(brand) VALUES($1)', [req.body.brand])
+    .then((dbres) => {
+      console.log('query response: ', dbres);
+      res.redirect('/');
+    })
+    .catch((err) => {
+      console.error('error running query', err);
+    });
 });
 
 module.exports = router;

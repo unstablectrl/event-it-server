b0ff3e11f65925b10ddd423750efbd36247dcb46
Unstable <unstablectrl@gmail.com>
2017-05-15 00:51:05 +0100

    Add firebaseui
    This change replaces the manually implemented signin/signup code

--- controllers/home.js
+++ controllers/home.js
@@ -4,3 +4,7 @@ module.exports.home = (req, res) => {
     description: 'Curabitur et magna diam. Etiam mauris ex, luctus eu tempus iaculis, malesuada eget libero. Integer luctus porttitor metus, sed imperdiet elit venenatis iaculis. Aenean tincidunt arcu non tellus rutrum cursus.'
   })
 }
+
+module.exports.signin = (req, res) => {
+  res.render('sign-in')
+}
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ let ctrlOther = require('../controllers/other')
 
 /* Home pages */
 router.get('/', ctrlHome.home)
+router.get('/sign-in', ctrlHome.signin)
 
 /* About pages */
 router.get('/about', ctrlOther.about)
